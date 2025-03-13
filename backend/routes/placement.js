f25aac9050e12b86ae7bfb536c7db072f6b86e8a
Saurya Gur <saurya.gur@outlook.com>
2025-03-13 14:33:50 +0530

    initialised backend

---
+++ backend/routes/placement.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var router = express.Router();
+
+/* POST /api/placement */
+router.get('/', function (req, res, next) {
+    res.json(["placement"]);
+});
+
+// Request Body:
+// {
+//     "items"
+// :
+//     [
+//         {
+//             "itemId": "string",
+//             "name": "string",
+//             "width": number,
+//             "depth": number,
+//             "height": number,
+//             "priority": number,
+//             "expiryDate": "string",// ISO format
+//             "usageLimit": number,
+//             "preferredZone": "string"// Zone
+//         }
+//     ],
+//         "containers"
+// :
+//     [
+//         {
+//             "containerId": "string",
+//             "zone": "string",
+//             "width": number,
+//             "depth": number,
+//             "height": number
+//         }
+//     ]
+// }
+
+module.exports = router;
+
+// Response body:
+// {
+//     "success"
+// :
+//     boolean,
+//         "placements"
+// :
+//     [
+//         {
+//             "itemId": "string",
+//             "containerId": "string",
+//             "position": {
+//                 "startCoordinates": {
+//                     "width": number,
+//                     "depth": number,
+//                     "height": number
+//                 },
+//                 "endCoordinates": {
+//                     "width": number,
+//                     "depth": number,
+//                     "height": number
+//                 }
+//             }
+//         }
+//     ],
+//         "rearrangements"
+// :
+//     [
+//         {
+//             "step": number,
+//             "action": "string",// "move", "remove","place"
+//             "itemId": "string",
+//             "fromContainer": "string",
+//             "fromPosition": {
+//                 "startCoordinates": {
+//                     "width": number,
+//                     "depth": number,
+//                     "height": number
+//                 },
+//                 "endCoordinates": {
+//                     "width": number,
+//                     "depth": number,
+//                     "height": number
+//                 }
+//             },
+//             "toContainer": "string",
+//             "toPosition": {
+//                 "startCoordinates": {
+//                     "width": number,
+//                     "depth": number,
+//                     "height": number
+//                 },
+//                 "endCoordinates": {
+//                     "width": number,
+//                     "depth": number,
+//                     "height": number
+//                 }
+//             }
+//         }
+//     ],
+// }
