7e3f84629467793db88c85a0a537164d9f3aaa6d
unknown <kukrejakush@gmail.com>
2025-03-17 00:48:41 +0530

    Renamed

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import {Outlet} from 'react-router-dom';
-import Sidebar from './Sidebar.tsx';
+import Sidebar from './sidebar.tsx';
 import React from "react";
 
 export default function Layout() {
